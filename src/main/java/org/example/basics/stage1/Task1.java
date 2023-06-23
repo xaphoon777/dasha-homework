691983c90b2ae9a1bd2944354774f7ad3fc3c045
Волобуев Александр <aleksandr.volobuev@gazprombank.ru>
2023-06-23 16:22:40 +0300

    Подсказка

--- src/main/java/org/example/basics/stage1/Task1.java
+++ src/main/java/org/example/basics/stage1/Task1.java
@@ -3,18 +3,19 @@ package org.example.basics.stage1;
 public class Task1 {
     /**
      * add two numbers
-     * @param first
-     * @param second
-     * @return - sum
+     * @param first - первое слагаемое
+     * @param second - второе слагаемое
+     * @return - сумма двух чисел
      */
     public static int sumTwoNumbers (int first, int second) {
         return first + second;
     }
 
     /**
-     * печатает "привет имя" (например Привет, Петя)
+     * Возвращает "Привет, имя !" (например Привет, Петя)
+     * (подсказка: печатать через System.out.println() ничего не надо. Надо только вернуть
       * @param name - имя
-     * @return
+     * @return - "Привет, имя !"
      */
     public static String sayHello(String name) {
         return System.out.println (Привет, ) + name + System.out.println(!);
